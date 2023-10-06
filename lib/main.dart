2c20dc30729216b5a8d17f0320b4fc7e682bc51b
KamranHussen71 <140729209+KamranHussen71@users.noreply.github.com>
2023-10-07 01:03:38 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SizeSelectionScreen(),
+      home: SizeSelectorScreen(),
     );
   }
 }
 
-class SizeSelectionScreen extends StatelessWidget {
+class SizeSelectorScreen extends StatefulWidget {
+  @override
+  _SizeSelectorScreenState createState() => _SizeSelectorScreenState();
+}
+
+class _SizeSelectorScreenState extends State<SizeSelectorScreen> {
+  String selectedSize = "";
+
+  void _onSizeButtonPressed(String size) {
+    setState(() {
+      selectedSize = size;
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Selected Size: $size'),
+      ),
+    );
+  }
+
+  Widget _buildSizeButton(String size) {
+    return ElevatedButton(
+      onPressed: () => _onSizeButtonPressed(size),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        primary: size == selectedSize ? Colors.orangeAccent : Colors.white10,
+        padding: EdgeInsets.symmetric(vertical: size == "S" ? 10 : 10, horizontal: 40),
+      ),
+      child: Text(
+        size,
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,34 +60,25 @@ class SizeSelectionScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            OutlinedButton(
-              child: Text(
-                "Outlined Button",
-                style: TextStyle(
-                  color: Colors.green,
-                ),
-              ),
-              onPressed: () {},
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildSizeButton("S"),
+                _buildSizeButton("M"),
+                _buildSizeButton("L"),
+                _buildSizeButton("XL"),
+              ],
             ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  _buildSizeButton("XXL"),
+                  _buildSizeButton("XXXL"),
+                ],
+              ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class SizeButton extends StatelessWidget {
-  final String size;
-
-  SizeButton({required this.size});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-
-      },
-      child: Text(size),
     );
   }
 }
